21f30ea21012b438e3499658e9887f9e69445139
Juan Camilo <jcvc_2000@hotmail.com>
2014-04-04 15:51:19 -0500

    Mi primer commit de mis configuraciones :D

---
+++ awesome/archmenu.lua
@@ -0,0 +1,73 @@
+ local menuf9c59cf15f7e3fd54a418134ad8a8be6 = {
+     {"7-Zip FM", "7zFM", "/usr/share/icons/hicolor/32x32/apps/p7zip.png" },
+     {"Busqueda de archivos de SpaceFM", "spacefm --find-files ", "/usr/share/icons/hicolor/48x48/apps/spacefm-find.png" },
+     {"ClipIt", "clipit"},
+     {"Galculator", "galculator", "/usr/share/icons/hicolor/48x48/apps/galculator.png" },
+     {"Gestor de archivadores", "file-roller ", "/usr/share/icons/hicolor/16x16/apps/file-roller.png" },
+     {"LXTerminal", "lxterminal", "/usr/share/pixmaps/lxterminal.png" },
+     {"Leafpad", "leafpad ", "/usr/share/icons/hicolor/16x16/apps/leafpad.png" },
+     {"Vi IMproved", "gvim -f ", "/usr/share/pixmaps/gvim.png" },
+     {"Xfburn", "xfburn", "/usr/share/icons/gnome/16x16/devices/media-cdrom.png" },
+ }
+
+ local menue2fa0f6ba17f6bf3d1bf1dbed5a1c5b2 = {
+     {"Viewnior", "viewnior ", "/usr/share/icons/hicolor/16x16/apps/viewnior.png" },
+     {"Visor de documentos", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+ }
+
+ local menu010ccd73d8965edb946094904b05ace3 = {
+     {"BleachBit", "bleachbit", "/usr/share/pixmaps/bleachbit.png" },
+     {"Editor de dconf", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/dconf-editor.png" },
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"Navegador Zeroconf de Avahi", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Sakura", "sakura", "/usr/share/pixmaps/terminal-tango.svg" },
+     {"SpaceFM", "spacefm ", "/usr/share/icons/hicolor/48x48/apps/spacefm.png" },
+     {"Terminal de Xfce", "xfce4-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"Terminator", "terminator", "/usr/share/icons/hicolor/16x16/apps/terminator.png" },
+     {"urxvt", "urxvt", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"urxvt (client)", "urxvtc", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"urxvt (tabbed)", "urxvt-tabbed", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+ }
+
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Cliente de mensajería de Internet Pidgin", "pidgin", "/usr/share/icons/hicolor/16x16/apps/pidgin.png" },
+     {"Examinador de servidores SSH de Avahi", "/usr/bin/bssh", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Examinador de servidores VNC de Avahi", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"GrooveOff", "grooveoff", "/usr/share/icons/hicolor/16x16/apps/grooveoff.png" },
+     {"Midori", "midori ", "/usr/share/icons/hicolor/16x16/apps/midori.png" },
+     {"Navegación privada Midori", "midori --private ", "/usr/share/icons/hicolor/16x16/apps/midori.png" },
+     {"dwb", "dwb ", "/usr/share/pixmaps/dwb.png" },
+ }
+
+ local menubd41283f290f7ee66eb3f4601b70a182 = {
+     {"Visor de documentos", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+     {"Zathura", "zathura "},
+ }
+
+ local menua417aafc9b2f25e6d396aadf7c37ab21 = {
+     {"CMake", "cmake-gui ", "/usr/share/pixmaps/CMakeSetup32.png" },
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
+ }
+
+ local menu7dca2558c58f0a1ad29089ca323f9a79 = {
+     {"Audacious", "audacious ", "/usr/share/icons/hicolor/48x48/apps/audacious.png" },
+     {"GrooveOff", "grooveoff", "/usr/share/icons/hicolor/16x16/apps/grooveoff.png" },
+     {"PNMixer", "pnmixer", "/usr/share/icons/pnmixer.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"Xfburn", "xfburn", "/usr/share/icons/gnome/16x16/devices/media-cdrom.png" },
+ }
+
+xdgmenu = {
+    {"Accesorios", menuf9c59cf15f7e3fd54a418134ad8a8be6},
+    {"Gráficos", menue2fa0f6ba17f6bf3d1bf1dbed5a1c5b2},
+    {"Herramientas del sistema", menu010ccd73d8965edb946094904b05ace3},
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"Oficina", menubd41283f290f7ee66eb3f4601b70a182},
+    {"Programación", menua417aafc9b2f25e6d396aadf7c37ab21},
+    {"Sonido y vídeo", menu7dca2558c58f0a1ad29089ca323f9a79},
+}
+
